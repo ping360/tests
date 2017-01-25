2df562247e0ef3158d5b6c3f247431dbd7636e12
ping360 <ping360p@gmail.com>
2017-01-25 19:18:43 +0200

    lesson namedQueries w shorobura

--- Library/src/main/java/ua/com/library/entity/Author.java
+++ Library/src/main/java/ua/com/library/entity/Author.java
@@ -6,7 +6,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
+
+@NamedQueries({
+		@NamedQuery(name = "findAllAuthor", query = "select a from Author a"),
+		@NamedQuery(name = "findAuthorBySurname", query = "select a from Author a where a.surname =:surname")
+})
+
 
 @Entity
 public class Author {
