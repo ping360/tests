45b9b61f68330bcdac4859b6c3f9edf23b282bbf
ping360 <ping360p@gmail.com>
2017-01-27 20:24:18 +0200

    delete methods DAO

--- Library/src/main/java/ua/com/library/dao/BookDao.java
+++ Library/src/main/java/ua/com/library/dao/BookDao.java
@@ -3,12 +3,12 @@ package ua.com.library.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
 
 import ua.com.library.entity.Author;
 import ua.com.library.entity.Book;
+import ua.com.library.entity.User;
 
-public class BookDao{
+public class BookDao {
 
 	final EntityManager manager;
 
@@ -33,7 +33,8 @@ public class BookDao{
 
 	public Book finOne(String title) {
 
-		Book book = (Book) manager.createQuery("select b from Book b where b.title =:param")
+		Book book = (Book) manager
+				.createQuery("select b from Book b where b.title =:param")
 				.setParameter("param", title).getSingleResult();
 
 		return book;
@@ -45,32 +46,89 @@ public class BookDao{
 		manager.remove(finOne(title));
 		manager.getTransaction().commit();
 	}
+
+	public void addAuthorToBook(String bookName, String authorName) {
+		manager.getTransaction().begin();
+
+		Author author = (Author) manager
+				.createQuery("select a from Author a where a.name =:param")
+				.setParameter("param", authorName).getSingleResult();
+
+		Book book = (Book) manager
+				.createQuery("select b from Book b where b.title =:param")
+				.setParameter("param", bookName).getSingleResult();
+
+		book.setAuthor(author);
+
+		manager.getTransaction().commit();
+	}
+
+	
+	/*  
+	    TESTING
+		TESTING
+		TESTING
+		TESTING
+	*/
 	
 	
-	public void addAuthorToBook(String bookName, String authorName){
+	//many to many
+	
+	public void deleteBookFromUser(String username, String bookName){
 		manager.getTransaction().begin();
 		
-		Author author = (Author) manager.
-				createQuery("select a from Author a where a.name =:param").
-				setParameter("param", authorName).
-				getSingleResult();
 		
-		
-		Book book = (Book) manager.
-				createQuery("select b from Book b where b.title =:param").
-				setParameter("param", bookName)
+		User user = (User) manager
+				.createQuery("select u from User u where u.surname =:surname")
+				.setParameter("surname", username)
 				.getSingleResult();
 		
-		book.setAuthor(author);
+		Book book = (Book) manager
+				.createQuery("select b from Book where b.title =:title")
+				.setParameter("title", bookName)
+				.getSingleResult();
 		
+		book.getUsers().remove(user);
+		// or
+		user.getBooks().remove(book);
+		
+		manager.getTransaction().commit();
+	}
+
+	
+	//one to many
+	
+	public void deleteAuthorFromBook(String bookName){
+		manager.getTransaction().begin();
+		
+		Book book = (Book) manager
+				.createQuery("select b from Book where b.title =:title")
+				.setParameter("title", bookName)
+				.getSingleResult();
+		
+		book.setAuthor(null);
 		
 		manager.getTransaction().commit();
 	}
 	
+	//delete Author and save books
 	
+	public void deleteAllBooksFromAuthor(String surnname){
+		manager.getTransaction().begin();
+		
+		Author author = (Author) manager
+				.createQuery("select a from Author a where a.surname :=surname")
+				.setParameter("surname", surnname)
+				.getSingleResult();
+		
+		for (Book book : author.getBooks()) {
+			book.setAuthor(null);
+			manager.merge(book);
+		}
+		
+		manager.remove(author);
+		
+		manager.getTransaction().commit();
+	}
 	
-	
-	
-	
-
 }
